4b424f384d5a43e7fc606e3716389e09e1b4bbd8
Amaya <guillaume.le-berre@epitech.eu>
2019-12-03 02:37:20 +0100

    [ADD] files for parsing file

---
+++ lib/inc/Parser/Parser.hpp
@@ -0,0 +1,29 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** Parser
+*/
+
+#ifndef PARSER_HPP_
+#define PARSER_HPP_
+
+    # include "LoadFile.hpp"
+    # include "File.hpp"
+
+namespace fileManager {
+
+    class Parser {
+
+        public:
+
+            Parser() = default;
+            ~Parser() = default;
+
+        private:
+
+    };
+
+}
+
+#endif /* !PARSER_HPP_ */
---
+++ lib/src/Parser/Parser.cpp
@@ -0,0 +1,11 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** Parser
+*/
+
+#include "Parser.hpp"
+
+namespace fileManager {
+}
