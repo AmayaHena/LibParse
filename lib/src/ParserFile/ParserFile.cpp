f5939527be759e360aa6711b4c6f91b8145b976f
Amaya <guillaume.le-berre@epitech.eu>
2019-12-04 23:39:26 +0100

    [UP] search for string(s) in a file

--- lib/src/ParserFile/ParserFile.cpp
+++ lib/src/ParserFile/ParserFile.cpp
@@ -38,8 +38,40 @@ namespace fileManager {
         return r;
     }
 
+    std::string ParserFile::find(std::string &s, std::vector<std::string> v, unsigned int i)
+    {
+        std::string ec;
+        if (i > v.size())
+            return ec;
+
+        while (i < v.size()) {
+            if (v[i].find(s, 0) != std::string::npos)
+                return v[i];
+            i++;
+        }
+        return ec;
+    }
+
+    std::vector<std::string> ParserFile::findA(std::string &s, std::vector<std::string> v)
+    {
+        std::vector<std::string> r;
+
+        for (unsigned int i = 0; i < v.size(); i++)
+            if (v[i].find(s, 0) != std::string::npos)
+                r.push_back(v[i]);
+        return r;
+    }
+
     std::vector<std::string> ParserFile::cleanContent(File &f) { return cleanVector(f.getContent()); }
 
     std::vector<std::string> ParserFile::cleanContent(std::vector<std::string> v) { return cleanVector(v); }
+
+    std::string ParserFile::findStr(std::string s, File f, unsigned int i) { return find(s, f.getContent(), i); }
+
+    std::string ParserFile::findStr(std::string s, std::vector<std::string> v, unsigned int i) { return find(s, v, i); }
+
+    std::vector<std::string> ParserFile::findAStr(std::string s, File f) { return findA(s, f.getContent()); }
+
+    std::vector<std::string> ParserFile::findAStr(std::string s, std::vector<std::string> v) { return findA(s, v); }
 
 }
