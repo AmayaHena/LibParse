c7968269603b3e0f6b74badafdc35f3697fc692b
Amaya <guillaume.le-berre@epitech.eu>
2019-12-09 06:53:04 +0100

    [ADD] reorganization

---
+++ lib/fileManager/src/File.cpp
@@ -0,0 +1,105 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** File
+*/
+
+#include "File.hpp"
+
+#include <fstream>
+
+namespace fileManager {
+
+    File::File(std::string &path) : _path(path) { parsingFile(); }
+
+    bool File::setPath(std::string &path)
+    {
+        if (path.empty())
+            return false;
+        cleanRessources();
+        _path = path;
+        return parsingFile();
+    }
+
+    std::string File::getExtension()
+    {
+        return _extension;
+    }
+
+    std::string File::getName()
+    {
+        return _name;
+    }
+
+    std::string File::getPath()
+    {
+        return _path;
+    }
+
+    std::vector<std::string> File::getContent()
+    {
+        return _content;
+    }
+
+    void File::cleanRessources()
+    {
+        _extension.clear();
+        _name.clear();
+        _path.clear();
+        _content.clear();
+    }
+
+    bool File::loadFile()
+    {
+        std::fstream f(_path);
+
+        if (!f)
+            return false;
+        std::string tmp;
+
+        while(std::getline(f, tmp))
+            _content.push_back(tmp);
+        f.close();
+        return true;
+    }
+
+    bool File::parseInfo()
+    {
+        std::size_t point = _path.find(".");
+        std::size_t slash = _path.find("/");
+
+        if (slash == std::string::npos && point == std::string::npos) {
+            _name = _path;
+            _extension.clear();
+            return true;
+        }
+
+        if (point != std::string::npos)
+            _extension = _path.substr(point, _path.length());
+        else
+            _extension = nullptr;
+
+        if (point != std::string::npos && slash == std::string::npos) {
+            _name = _path.substr(0, point);
+            return true;
+        }
+
+        for (slash = _path.length(); _path[slash] != '/'; slash--);
+        for (point = _path.length(); _path[point] != '.'; point--);
+        _name = _path.substr(slash + 1, point - slash - 1);
+        _extension = _path.substr(point, _path.length());
+        if (_extension[1] == '/')
+            _extension.clear();
+        return true;
+    }
+
+    bool File::parsingFile()
+    {
+        if (!loadFile())
+            return false;
+        parseInfo();
+        return true;
+    }
+
+}
---
+++ lib/fileManager/src/LoadFile.cpp
@@ -0,0 +1,132 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** LoadFile
+*/
+
+# include "LoadFile.hpp"
+
+namespace fileManager {
+
+    LoadFile::LoadFile(std::string path) { load(path); }
+
+    LoadFile::LoadFile(std::vector<std::string> path) { load(path); }
+
+    std::vector<File> LoadFile::getAllFile()
+    {
+        return _container;
+    }
+
+    File &LoadFile::getFileByExtension(std::string extension, unsigned int position)
+    {
+        for (unsigned int i = position; i < _container.size(); i++)
+            if (_container[i].getExtension() == extension)
+                return _container.at(i);
+        return _container.at(position);
+    }
+
+    std::vector<File> LoadFile::getAFileByExtension(std::string extension)
+    {
+        std::vector<File> v;
+
+        for (File &f : _container)
+            if (f.getExtension() == extension)
+                v.push_back(f);
+        return v;
+    }
+
+    File &LoadFile::getFileByName(std::string name, unsigned int position)
+    {
+        for (unsigned int i = position; i < _container.size(); i++)
+            if (_container[i].getName() == name)
+                return _container.at(i);
+        return _container.at(position);
+    }
+
+    std::vector<File> LoadFile::getAFileByName(std::string name)
+    {
+        std::vector<File> v;
+        unsigned int i = 0;
+
+        for (File &f : _container) {
+            if (f.getName() == name)
+                v.push_back(_container.at(i));
+            i++;
+        }
+        return v;
+    }
+
+    File &LoadFile::getFileByPath(std::string path)
+    {
+        for (File &f : _container)
+            if (f.getPath() == path)
+                return f;
+        return _container.at(0);
+    }
+
+    int unsigned LoadFile::getFileNumber() { return _container.size(); }
+
+    int unsigned LoadFile::getFileNumberWExtension(std::string extension)
+    {
+        unsigned int i = 0;
+
+        for (File &f : _container)
+            if (f.getExtension() == extension)
+                i++;
+        return i;
+    }
+
+    int unsigned LoadFile::getFileNumberWName(std::string name)
+    {
+        unsigned int i = 0;
+
+        for (File &f : _container)
+            if (f.getName() == name)
+                i++;
+        return i;
+    }
+
+    File &LoadFile::operator[](unsigned int position)
+    {
+        return _container.at(position);
+    }
+
+    bool LoadFile::addFile(std::string &path)
+    {
+        for (unsigned int i = 0; i < _container.size(); i++) {
+            if (_container[i].getPath() == path) {
+                _container[i].setPath(path);
+                return true;
+            }
+        }
+
+        File f;
+
+        if (!f.setPath(path))
+            return false;
+        _container.push_back(std::move(f));
+        return true;
+    }
+
+    void LoadFile::delFile(unsigned int position)
+    {
+        _container.erase(_container.begin() + position);
+    }
+
+    bool LoadFile::load(std::string path)
+    {
+        if (!addFile(path))
+            return false;
+        return true;
+    }
+
+    bool LoadFile::load(std::vector<std::string> path)
+    {
+        for (std::string &s : path)
+            if (!addFile(s))
+                return false;
+        return true;
+    }
+
+}
