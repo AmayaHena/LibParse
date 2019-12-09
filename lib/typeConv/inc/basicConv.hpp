a4a08cf3f56c8ea5c6a25a952d71ead6f222a623
Amaya <guillaume.le-berre@epitech.eu>
2019-12-09 07:20:09 +0100

    [ADD] typeConv lib

---
+++ lib/typeConv/inc/basicConv.hpp
@@ -0,0 +1,25 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** basicConv
+*/
+
+#ifndef BASICCONV_HPP_
+#define BASICCONV_HPP_
+
+namespace typeConv {
+
+    class basicConv {
+
+        public:
+
+            basicConv() = default;
+            ~basicConv() = default;
+
+        private:
+
+    };
+}
+
+#endif /* !BASICCONV_HPP_ */
---
+++ lib/typeConv/src/basicConv.cpp
@@ -0,0 +1,14 @@
+/*
+** PERSONAL PROJECT, 2019
+** LibParse
+** File description:
+** basicConv
+*/
+
+
+namespace typeConv {
+
+    class basicConv {
+
+    };
+}
