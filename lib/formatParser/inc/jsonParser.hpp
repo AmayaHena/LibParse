b9f72a28a87b44e48f1c63680a497e4b2d02f684
Amaya <guillaume.le-berre@epitech.eu>
2019-12-19 09:00:03 +0100

    [ADD] formatParser

---
+++ lib/formatParser/inc/jsonParser.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** LibParse
+** File description:
+** jsonParser
+*/
+
+#ifndef JSONPARSER_HPP_
+#define JSONPARSER_HPP_
+
+namespace formatParser {
+
+    class jsonParser {
+
+        public:
+
+        private:
+
+    };
+
+}
+
+#endif /* !JSONPARSER_HPP_ */
---
+++ lib/formatParser/src/jsonParser.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2019
+** LibParse
+** File description:
+** jsonParser
+*/
+
+#include "jsonParser.hpp"
