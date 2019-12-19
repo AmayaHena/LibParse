e4f50b7529f98d6b45ab2b91883f736044d62866
Amaya <guillaume.le-berre@epitech.eu>
2019-12-19 09:41:39 +0100

    [UP] basic functions

--- lib/formatParser/src/jsonParser.cpp
+++ lib/formatParser/src/jsonParser.cpp
@@ -6,3 +6,46 @@
 */
 
 #include "jsonParser.hpp"
+
+namespace formatParser {
+
+    std::unordered_map<std::string, std::string> jsonParser::parse(fileManager::File f) { return parseAD(f.getContent()); }
+
+    std::unordered_map<std::string, std::string> jsonParser::parse(std::vector<std::string> v) { return parseAD(v); }
+
+    std::unordered_map<std::string, std::string> jsonParser::parse(fileManager::File f, std::string match) { return parseS(f.getContent(), match); }
+
+    std::unordered_map<std::string, std::string> jsonParser::parse(std::vector<std::string> v, std::string match) { return parseS(v, match); }
+
+    std::string jsonParser::parseOne(fileManager::File f, std::string match) { return parseO(f.getContent(), match); }
+
+    std::string jsonParser::parseOne(std::vector<std::string> v, std::string match) { return parseO(v, match); }
+
+    std::unordered_map<std::string, std::string> jsonParser::parseAD(std::vector<std::string> v)
+    {
+        std::unordered_map<std::string, std::string> m;
+
+        if (v.empty())
+            return m;
+        return m;
+    }
+
+    std::unordered_map<std::string, std::string> jsonParser::parseS(std::vector<std::string> v, std::string match)
+    {
+        std::unordered_map<std::string, std::string> m;
+
+        if (v.empty() || match.empty())
+            return m;
+        return m;
+    }
+
+    std::string jsonParser::parseO(std::vector<std::string> v, std::string match)
+    {
+        std::string rez;
+
+        if (v.empty() || match.empty())
+            return rez;
+        return rez;
+    }
+
+}
