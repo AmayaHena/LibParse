5e585ebe99f2a37cd13eca0a6562e574a516e1ae
Amaya <guillaume.le-berre@epitech.eu>
2019-12-31 08:47:03 +0100

    [DEL] wont handle sections for nwo

--- lib/formatParser/inc/xmlParser.hpp
+++ lib/formatParser/inc/xmlParser.hpp
@@ -23,22 +23,14 @@ namespace formatParser {
         std::vector<std::pair<std::string, std::string>> parse(fileManager::File &f);
         std::vector<std::pair<std::string, std::string>> parse(std::vector<std::string> v);
 
-        /* Parse XML all Data */
-        std::vector<std::pair<std::string, std::string>> parse(fileManager::File &f, std::string match);
-        std::vector<std::pair<std::string, std::string>> parse(std::vector<std::string> v, std::string match);
-
-        void cutest(fileManager::File &f);
-
         private:
 
         /* Parsing tools */
         std::string getLValue(std::string s);
         std::string getRValue(std::vector<std::string> v, size_t i);
-        bool isSection(std::vector<std::string> v, size_t i);
 
         /* XML Parsing Core */
         std::vector<std::pair<std::string, std::string>> parseAD(std::vector<std::string> v);
-        std::vector<std::pair<std::string, std::string>> parseS(std::vector<std::string> v, std::string match);
 
     };
 
--- lib/formatParser/src/xmlParser.cpp
+++ lib/formatParser/src/xmlParser.cpp
@@ -14,37 +14,7 @@ namespace formatParser {
 
     std::vector<std::pair<std::string, std::string>> xmlParser::parse(std::vector<std::string> v) { return parseAD(v); }
 
-    std::vector<std::pair<std::string, std::string>> xmlParser::parse(fileManager::File &f, std::string match) { return parseS(f.getContent(), match); }
-
-    std::vector<std::pair<std::string, std::string>> xmlParser::parse(std::vector<std::string> v, std::string match) { return parseS(v, match); }
-
     std::string xmlParser::getLValue(std::string s) { return s.substr(s.find_first_of("<") +1, s.find_first_of(">") -1); }
-
-    bool xmlParser::isSection(std::vector<std::string> v, size_t i)
-    {
-        size_t j = v[i].find_first_of(">") + 1;
-
-        while (i < v.size()) {
-            std::cout << "v[" << i << "]" << v[i] << std::endl;
-            //std::cout << std::endl << v[i] << std::endl << std::endl;
-            while (j < v[i].size()) {
-                if (v[i][j] == '<'
-                && ((j == 0) || (v[i][j - 1] == ' '))) {
-                std::cout << "TRUE" << std::endl;
-                std::cout << "v["<<i<<"]["<<j<<"] " << v[i][j] << std::endl << "---" << std::endl;
-                    return true;
-                }
-                if (v[i][j] != ' ') {
-//                std::cout << "1 v["<<i<<"]["<<j<<"] " << v[i][j];
-                    return false;
-                }
-                j++;
-            }
-            j = 0;
-            i++;
-        }
-        return false;
-    }
 
     std::string xmlParser::getRValue(std::vector<std::string> v, size_t i)
     {
@@ -85,39 +55,6 @@ namespace formatParser {
             }
         }
         return rez;
-    }
-
-    std::vector<std::pair<std::string, std::string>> xmlParser::parseS(std::vector<std::string> v, std::string match)
-    {
-        std::vector<std::pair<std::string, std::string>> rez;
-
-        if (v.empty())
-            return rez;
-
-        size_t i = 0;
-
-        while (v[i++].find("<" + match) == std::string::npos);
-
-        while (i < v.size() || v[i++].find("</" + match + ">") == std::string::npos) {
-            if (!v[i].empty()) {
-                rez.push_back(make_pair(getLValue(v[i]), getRValue(v, i)));
-                if (v[i].find("</") == std::string::npos) {
-                    while (v[i++].find("</") == std::string::npos);
-                    i--;
-                }
-            }
-        }
-        return rez;
-    }
-
-
-    void xmlParser::cutest(fileManager::File &f)
-    {
-        std::vector<std::string> v = f.getContent();
-
-        for (size_t i = 0; i < v.size(); i++) {
-            isSection(v, i);
-        }
     }
 
 }
