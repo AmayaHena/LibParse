e5bd20e2babd1571d4484be52871f39fbb642017
Amaya <guillaume.le-berre@epitech.eu>
2020-07-06 19:04:46 +0200

    [UP] rename var

--- lib/FileManager/inc/Parser.hpp
+++ lib/FileManager/inc/Parser.hpp
@@ -20,7 +20,7 @@ namespace FileManager {
 
             /* Clean */
             inline std::vector<std::string> clean(File &f) { return cleanV(f.getContent()); }
-            inline std::vector<std::string> clean(std::vector<std::string> &v) { return cleanV(v); }
+            inline std::vector<std::string> clean(const std::vector<std::string> &v) { return cleanV(v); }
 
             /* Parse */
             inline std::string find(const std::string &s, File &f, size_t i = 0)  { return findS(s, f.getContent(), i); }
