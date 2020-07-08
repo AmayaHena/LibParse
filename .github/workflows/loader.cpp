b3dbea49f8d3020bd08621987646a3f272201c15
Amaya <guillaume.le-berre@epitech.eu>
2020-07-08 22:48:52 +0200

    [ADD] loader test

---
+++ .github/workflows/loader.cpp
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2020
+** LibParse
+** File description:
+** loader
+*/
+
+#include "../../lib/FileManager/inc/Loader.hpp"
+
+int main()
+{
+    FileManager::Loader f;
+
+    /* TEST DEL EC */
+    if (f.delFile(0))
+        return 84;
+    if (f.delFile(15))
+        return 84;
+    if (f.delFile("kappa.txt"))
+        return 84;
+
+    std::cout << "DEL EC : OK" << std::endl;
+
+    /* TEST LOAD EC */
+    if (f.load("../ec.txt"))
+        return 84;
+    if (f.getFileNb() != 0)
+        return 84;
+
+    std::cout << "LOAD EC : OK" << std::endl;
+
+    /* TEST LAOD */
+    if (!f.load("../../CMakeLists.txt"))
+        return 84;
+    if (f.getFileNb() != 1)
+        return 84;
+    if (!f.load("../../.gitignore"))
+        return 84;
+    if (f.getFileNb() != 2)
+        return 84;
+    if (!f.load("../../lib/FileManager/inc/File.hpp"))
+        return 84;
+    if (f.getFileNb() != 3)
+        return 84;
+
+    std::cout << "LOAD : OK" << std::endl;
+
+    /* TEST ISFILE */
+
+    if (f.isFile("../../ec.txt"))
+        return 84;
+    if (!f.isFile("../../CMakeLists.txt"))
+        return 84;
+    if (!f.isFile("../../.gitignore"))
+        return 84;
+    if (!f.isFile("../../lib/FileManager/inc/File.hpp"))
+        return 84;
+
+    std::cout << "ISFILE : OK" << std::endl;
+
+    /* TEST DEL */
+
+    if (f.delFile(89))
+        return 84;
+    if (f.delFile("ec.txt"))
+        return 84;
+    if (f.getFileNb() != 3)
+        return 84;
+    if (!f.delFile())
+        return 84;
+    if (f.getFileNb() != 2)
+        return 84;
+    if (!f.delFile("../../.gitignore"))
+        return 84;
+    if (f.getFileNb() != 1)
+        return 84;
+
+    if (f.isFile("../../CMakeLists.txt"))
+        return 84;
+    if (f.isFile("../../.gitignore"))
+        return 84;
+    if (!f.isFile("../../lib/FileManager/inc/File.hpp"))
+        return 84;
+
+    std::cout << "DEL : OK" << std::endl;
+
+    return 0;
+}
