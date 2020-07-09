f2bedea638b58572e618a713a9e06ceb6d1ed41e
Amaya <guillaume.le-berre@epitech.eu>
2020-07-09 17:17:46 +0200

    [UP] more tests for file

--- .github/workflows/file.cpp
+++ .github/workflows/file.cpp
@@ -5,22 +5,54 @@
 ** main
 */
 
+#include <fstream>
+
+#include <vector>
+#include <iostream>
+
 #include "../../lib/FileManager/inc/File.hpp"
+
+std::vector<std::string> getFile(std::string s)
+{
+    std::vector<std::string> v;
+    std::fstream f(s);
+    if (!f)
+        return v;
+
+    std::string tmp;
+    while(std::getline(f, tmp))
+        v.push_back(tmp);
+    f.close();
+    return v;
+}
 
 int main()
 {
-    /* TESTTING LOAD */
+    /* INIT */
     FileManager::File f("../../CMakeLists.txt");
 
     FileManager::File f1;
     FileManager::File f2;
+
+    /* TESTTING LOAD EC */
+
+    if (f1.setPath("cuties.txt"))
+        return 84;
+    if (f2.setPath("waifus/mostimo.OWO"))
+        return 84;
+
+    std::cout << "LOAD EC : \t[\033[1;32mOK\033[0m]" << std::endl;
+
+    /* TESTTING LOAD */
 
     if (!f1.setPath("../../.gitignore"))
         return 84;
     if (!f2.setPath("../../lib/FileManager/inc/File.hpp"))
         return 84;
 
-    std::cout << "LOAD : OK" << std::endl;
+    std::cout << "LOAD : \t[\033[1;32mOK\033[0m]" << std::endl;
+
+    /* TESTTING EXT */
 
     if (f.getExtension() != "txt")
         return 84;
@@ -29,7 +61,9 @@ int main()
     if (f2.getExtension() != "hpp")
         return 84;
 
-    std::cout << "EXT : OK" << std::endl;
+    std::cout << "EXT : \t[\033[1;32mOK\033[0m]" << std::endl;
+
+    /* TESTTING PATH */
 
     if (f.getPath() != "../../CMakeLists.txt")
         return 84;
@@ -38,7 +72,9 @@ int main()
     if (f2.getPath() != "../../lib/FileManager/inc/File.hpp")
         return 84;
 
-    std::cout << "PATH : OK" << std::endl;
+    std::cout << "PATH : \t[\033[1;32mOK\033[0m]" << std::endl;
+
+    /* TESTTING NAME */
 
     if (f.getName() != "CMakeLists")
         return 84;
@@ -47,7 +83,42 @@ int main()
     if (f2.getName() != "File")
         return 84;
 
-    std::cout << "NAME : OK" << std::endl;
+    std::cout << "NAME : \t[\033[1;32mOK\033[0m]" << std::endl;
+
+    /* TESTTING CONTENT */
+
+    std::vector<std::string> vt = f.getContent();
+    std::vector<std::string> vo = getFile(f.getPath());
+
+    if (vt.size() != vo.size())
+        return 84;
+    for (size_t it = 0; it < vt.size(); it++)
+        if (vt[it] != vo[it])
+            return 84;
+    vt.clear();
+    vo.clear();
+
+    vt = f1.getContent();
+    vo = getFile(f1.getPath());
+    if (vt.size() != vo.size())
+        return 84;
+    for (size_t it = 0; it < vt.size(); it++)
+        if (vt[it] != vo[it])
+            return 84;
+    vt.clear();
+    vo.clear();
+
+    vt = f2.getContent();
+    vo = getFile(f2.getPath());
+    if (vt.size() != vo.size())
+        return 84;
+    for (size_t it = 0; it < vt.size(); it++)
+        if (vt[it] != vo[it])
+            return 84;
+    vt.clear();
+    vo.clear();
+
+    std::cout << "CONTENT : \t[\033[1;32mOK\033[0m]" << std::endl;
 
     return 0;
 }
