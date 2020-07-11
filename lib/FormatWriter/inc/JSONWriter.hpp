e8b7aefd7c50b29a62c1915f7302e0c18d33d4ed
Amaya <guillaume.le-berre@epitech.eu>
2020-07-11 22:19:26 +0200

    [FIX] arch formatwriter

---
+++ lib/FormatWriter/inc/JSONWriter.hpp
@@ -0,0 +1,20 @@
+/*
+** PERSONAL PROJECT, 2020
+** Nyx
+** File description:
+** JSONWriter
+*/
+
+#pragma once
+
+namespace FormatWriter {
+
+    class JSONWriter final {
+
+        public:
+
+            JSONWriter() = default;
+
+    };
+
+}
---
+++ lib/FormatWriter/src/JSONWriter.cpp
@@ -0,0 +1,12 @@
+/*
+** PERSONAL PROJECT, 2020
+** Nyx
+** File description:
+** JSONWriter
+*/
+
+#include "JSONWriter.hpp"
+
+namespace FormatWriter {
+
+}
