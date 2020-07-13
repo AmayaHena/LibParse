29ff246cb24c5a27c05e3c96a19384c768f26225
Amaya <guillaume.le-berre@epitech.eu>
2020-07-13 22:22:25 +0200

    [UP] build

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-stty -raw
-stty echo
-stty onlcr
-
-if [ ! -d "./build" ]; then
-    mkdir build
-fi
+mkdir -p build
 cd build && cmake .. -G "Unix Makefiles" && cmake --build . && mv libNyx.so ..
