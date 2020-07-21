10eb50d1fa0d8735c1cea658442522356e8dcd55
Amaya <guillaume.le-berre@epitech.eu>
2020-07-22 00:44:43 +0200

    [UP] xml parser

--- lib/FormatParser/src/XMLParser.cpp
+++ lib/FormatParser/src/XMLParser.cpp
@@ -11,44 +11,79 @@ namespace FormatParser {
 
     /* PRIVATE METHODS */
 
-    std::string XMLParser::getLValue(const std::string &s) const { return s.substr(s.find_first_of("<") + 1, s.find_first_of(">") - 1); }
-
-    std::string XMLParser::getRValue(const std::vector<std::string> &v, size_t i) const
+    std::string XMLParser::getString(const std::string &s, const char b, const char e) const
     {
-        std::string s;
+        size_t ft = s.find(b) + 1;
 
-        if (v[i].find("</") != std::string::npos) {
-            return v[i].substr(v[i].find_first_of(">") + 1, v[i].find_last_of("<") - v[i].find_first_of(">") - 1);
-        } else {
-            s += v[i].substr(v[i].find_first_of(">") + 1, v[i].size() - 1);
-            if (v[i + 1].find("</") != std::string::npos) {
-                s += v[i + 1].substr(0, v[i + 1].find("</"));
-                return s;
-            }
-            while (v[i++].find("</") == std::string::npos) {
-                if (v[i].find("</") != std::string::npos)
-                    break;
-                s += v[i];
-            }
-        }
+        return s.substr(ft, s.find(e, ft) - ft);
+    }
 
-        return s;
+    char XMLParser::getCNb(const std::string s, char c) const
+    {
+        char it = 0;
+
+        for (char a : s)
+            if (a == c)
+                it++;
+        return it;
+    }
+
+    void XMLParser::makePair(const std::string &s, std::vector<std::pair<std::string, std::string>> &r) const
+    {
+        if (s.find("</") != std::string::npos
+        && getCNb(s, '<') == 2 && getCNb(s, '>') == 2)
+            r.push_back(make_pair(getLValue(s), getRValue(s)));
     }
 
     std::vector<std::pair<std::string, std::string>> XMLParser::parseAll(const std::vector<std::string> &v) const
     {
-        std::vector<std::pair<std::string, std::string>> rez;
+        std::vector<std::pair<std::string, std::string>> r;
 
-        for (size_t i = 0; i < v.size(); i++) {
-            if (!v[i].empty()) {
-                rez.push_back(make_pair(getLValue(v[i]), getRValue(v, i)));
-                if (v[i].find("</") == std::string::npos) {
-                    while (v[i++].find("</") == std::string::npos);
-                    i--;
-                }
-            }
-        }
-        return rez;
+        for (const std::string &s : v)
+            makePair(s, r);
+        return r;
+    }
+
+    std::vector<std::pair<std::string, std::string>> XMLParser::parseSection(const std::vector<std::string> &v, const std::string &match) const noexcept
+    {
+        std::vector<std::pair<std::string, std::string>> r;
+
+        if (match.empty())
+            return r;
+
+        size_t it = 0;
+
+        while (it < v.size())
+            if (v[it++].find("<" + match + ">") != std::string::npos)
+                break;
+        if (it == v.size())
+            return r;
+
+        while (v[it] != "<" + match + "/>")
+            makePair(v[it++], r);
+        return r;
+    }
+
+    std::string XMLParser::parseValue(const std::vector<std::string> &v, const std::string &match) const noexcept
+    {
+        if (match.empty())
+            return "";
+
+        for (const std::string &s : v)
+            if (s.find("<" + match + ">") != std::string::npos)
+                return getRValue(s);
+        return "";
+    }
+
+    std::vector<std::string> XMLParser::getSName(const std::vector<std::string> &v) const
+    {
+        std::vector<std::string> r;
+
+        for (const std::string &s : v)
+            if (s.find("/") == std::string::npos
+        && getCNb(s, '<') == 1 && getCNb(s, '>') == 1)
+                r.push_back(getLValue(s));
+        return r;
     }
 
 }
